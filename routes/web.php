8cb511b55a91b2fde7345b4aa595a83397913a09
unknown <fauziagustian68@gmail.com>
2025-05-14 17:06:33 +0700

    feat: add parachute page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\web\ParachuteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('web.dashboard.index');
+});
+
+Route::controller(ParachuteController::class)->prefix('/parachute')->group(function () {
+    Route::get('/', 'index')->name('outlet-book.index');
+    Route::post('/', 'store')->name('outlet-book.post');
+    Route::post('/{id}/update', 'update')->name('outlet-book.update');
+    Route::delete('/{id}', 'destroy')->name('outlet-book.destroy');
 });
