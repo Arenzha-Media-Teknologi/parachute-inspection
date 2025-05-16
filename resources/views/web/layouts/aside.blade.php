c83bca6856044894bb17f508f41ba77f2f774f49
skyra <rianaanggra01@gmail.com>
2025-05-16 16:07:08 +0700

    feat: create add users

--- resources/views/web/layouts/aside.blade.php
+++ resources/views/web/layouts/aside.blade.php
@@ -47,7 +47,7 @@
                 <div class="menu-item">
                     <a class="menu-link {{ request()->is('user-group*') ? 'active' : '' }}" href="/user-group">
                         <span class="menu-icon">
-                            <i class="ki-outline ki-user-square fs-2"></i>
+                            <i class="ki-outline ki-profile-user fs-2"></i>
                         </span>
                         <span class="menu-title">User Group</span>
                     </a>
