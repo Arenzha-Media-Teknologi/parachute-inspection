b8ce3c6329bf3854680018847e4004abddc9a9d8
royhanreza <royhanreza842@gmail.com>
2025-05-27 17:29:19 +0700

    style: change button color

--- resources/views/web/layouts/button/parachute-button.blade.php
+++ resources/views/web/layouts/button/parachute-button.blade.php
@@ -9,13 +9,13 @@ $permission = json_decode(Auth::user()->user_groups->permissions);
     <a href="#" class="btn btn-sm hover-scale btn-warning me-2 btn-edit-parasut"
         data-id="{{ $model->id }}"
         data-bs-toggle="modal"
-        data-bs-target="#kt_modal_edit">
+        data-bs-target="#kt_modal_edit" style="background-color: #607D8B">
         <i class="fas fa-pencil-alt fs-4 me-2"></i>Edit
     </a>
     @endif
 
     @if(in_array("delete_parachute", $permission))
-    <a href="#" data-id="{{ $model->id }}" class="btn btn-sm btn-danger hover-scale btn-delete" title="Hapus">
+    <a href="#" data-id="{{ $model->id }}" class="btn btn-sm btn-danger hover-scale btn-delete" style="background-color: #D32F2F" title="Hapus">
         <i class="fas fa-trash-alt fs-4 me-2"></i>Hapus
     </a>
     @endif
--- resources/views/web/layouts/button/parachute-inspection-button.blade.php
+++ resources/views/web/layouts/button/parachute-inspection-button.blade.php
@@ -7,13 +7,13 @@ $permission = json_decode(Auth::user()->user_groups->permissions);
 <div class="text-center">
     @if(in_array("edit_parachute_check", $permission))
     <!-- <a href="#" class="btn btn-sm btn-warning hover-scale btn-detail me-2" data-id="{{ $model->id }}" data-bs-toggle="modal" data-bs-target="#kt_modal_detail"> -->
-    <a href="/parachute-inspection/edit/{{ $model->id }}" class="btn btn-sm btn-warning hover-scale me-2" data-id="{{ $model->id }}">
+    <a href="/parachute-inspection/edit/{{ $model->id }}" class="btn btn-sm btn-warning hover-scale me-2" style="background-color: #607D8B" data-id="{{ $model->id }}">
         <i class="fas fa-file-alt fs-4 me-2"></i> Riwayat
     </a>
     @endif
 
     @if(in_array("delete_parachute_check", $permission))
-    <a href="#" data-id="{{ $model->id }}" class="btn btn-sm btn-danger hover-scale btn-delete" title="Hapus">
+    <a href="#" data-id="{{ $model->id }}" class="btn btn-sm btn-danger hover-scale btn-delete" style="background-color: #D32F2F" title="Hapus">
         <i class="fas fa-trash-alt fs-4 me-2"></i>Hapus
     </a>
     @endif
